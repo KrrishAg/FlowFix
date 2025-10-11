b52d4c81466169ee8adeca94893b0675f7a240ba
KRRISH AGARWAL <krrishapj@gmail.com>
2025-10-11 14:54:23 +0530

    processor(picking jobs from hooks) working

---
+++ zapier/apps/processor/src/index.ts
@@ -0,0 +1,40 @@
+import prisma from "@repo/db/client";
+import { Kafka } from "kafkajs";
+import { TOPIC } from "@repo/common/config";
+
+const kafka = new Kafka({
+  clientId: "new-client",
+  brokers: ["localhost:9092"],
+});
+
+async function main() {
+  const producer = kafka.producer();
+  await producer.connect();
+
+  while (1) {
+    const pendingRows = await prisma.zapRunOutbox.findMany({
+      where: {},
+      take: 10,
+    });
+
+    pendingRows.forEach((row, idx) => {
+      producer.send({
+        topic: TOPIC,
+        messages: pendingRows.map((r) => ({
+          value: r.zapRunId,
+        })),
+      });
+    });
+
+    //after putting them in kafka, deleting from the zaprunoutbox table
+    await prisma.zapRunOutbox.deleteMany({
+      where: {
+        id: {
+          in: pendingRows.map((r) => r.id),
+        },
+      },
+    });
+  }
+}
+
+main();
