808f6eaa4f9834bfed1a58496b4f6817cde035d7
KRRISH AGARWAL <krrishapj@gmail.com>
2025-10-11 13:19:20 +0530

    added monorepo

---
+++ zapier/apps/hooks/src/index.ts
@@ -0,0 +1,37 @@
+import prisma from "@repo/db/client";
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+  const userId = req.params.userId;
+  const zapId = req.params.zapId;
+  const body = req.body;
+
+  await prisma.$transaction(async (tx) => {
+    //storing in db
+    const run = await tx.zapRun.create({
+      data: {
+        zapId,
+        metadata: body, //this would be data sent in body in json format
+      },
+    });
+
+    //storing in the outbox as well
+    await tx.zapRunOutbox.create({
+      data: {
+        zapRunId: run.id,
+      },
+    });
+
+    //maybe push it onto a queue like redis or kafka
+  });
+
+  res.send("ZapRun added");
+});
+
+app.listen(3000, () => {
+  console.log("Hook server listening on port 3000");
+});
