cf6fd6f29836f39e0ab0759b493f534f54819a77
KRRISH AGARWAL <krrishapj@gmail.com>
2025-10-12 18:36:58 +0530

    get all action/trigger endpoints

--- zapier/apps/backend/src/index.ts
+++ zapier/apps/backend/src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import userRouter from "./routes/user.js";
 import zapRouter from "./routes/zap.js";
+import { triggerRouter } from "./routes/trigger.js";
+import { actionRouter } from "./routes/action.js";
 
 const app = express();
 app.use(express.json());
@@ -10,6 +12,10 @@ app.use(cors());
 app.use("/api/v1/user", userRouter);
 
 app.use("/api/v1/zap", zapRouter);
+
+app.use("/api/v1/trigger", triggerRouter);
+
+app.use("/api/v1/action", actionRouter);
 
 app.listen(3000, () => {
   console.log("Primary backend listening on prt 3000");
---
+++ zapier/apps/backend/src/routes/trigger.ts
@@ -0,0 +1,9 @@
+import prisma from "@repo/db/client";
+import express from "express";
+
+export const triggerRouter = express.Router();
+
+triggerRouter.get("/available", async (req, res) => {
+  const availableTriggers = await prisma.availableTrigger.findMany({});
+  res.json({ availableTriggers });
+});
