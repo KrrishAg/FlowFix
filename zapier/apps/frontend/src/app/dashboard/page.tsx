79610096b5d326bb675c06e449f5349c92eb8446
KRRISH AGARWAL <krrishapj@gmail.com>
2025-10-14 00:12:47 +0530

    added loader

---
+++ zapier/apps/frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,151 @@
+"use client";
+import { Appbar } from "@/components/Appbar";
+import { DarkButton } from "@/components/buttons/DarkButton";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BACKEND_URL, HOOKS_URL } from "../config";
+import { LinkButton } from "@/components/buttons/LinkButton";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+
+//the type of data returned by the backend, got from postman
+interface Zap {
+  id: string;
+  triggerId: string;
+  userId: number;
+  actions: {
+    id: string;
+    zapId: string;
+    actionId: string;
+    sortingOrder: number;
+    type: {
+      id: string;
+      name: string;
+      image: string;
+    };
+  }[];
+  trigger: {
+    id: string;
+    zapId: string;
+    triggerId: string;
+    type: {
+      id: string;
+      name: string;
+      image: string;
+    };
+  };
+}
+
+function useZaps() {
+  const [loading, setLoading] = useState(true);
+  const [zaps, setZaps] = useState<Zap[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(`${BACKEND_URL}/api/v1/zap`, {
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        },
+      })
+      .then((res) => {
+        setZaps(res.data.zaps);
+      })
+      .catch((err) => {
+        setError(err.response.data.error);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return {
+    loading,
+    zaps,
+    error,
+  };
+}
+
+export default function Page() {
+  const { loading, zaps, error } = useZaps();
+  const router = useRouter();
+
+  return (
+    <div>
+      {/* <Appbar /> */}
+      <div className="flex justify-center pt-8">
+        <div className="max-w-screen-lg	 w-full">
+          <div className="flex justify-between pr-8 ">
+            <div className="text-2xl font-bold">My Zaps</div>
+            <DarkButton
+              onClick={() => {
+                router.push("/zap/create");
+              }}
+            >
+              Create
+            </DarkButton>
+          </div>
+        </div>
+      </div>
+
+      {loading ? (
+        <div className="flex justify-center pt-20">
+          <div className="loader"></div>
+        </div>
+      ) : error ? (
+        <div className="text-red-500 text-xl font-semibold text-center mt-20">
+          {error}
+        </div>
+      ) : (
+        <div className="flex justify-center">
+          <ZapTable zaps={zaps} />
+        </div>
+      )}
+    </div>
+  );
+}
+
+function ZapTable({ zaps }: { zaps: Zap[] }) {
+  const router = useRouter();
+
+  return (
+    <div className="p-8 max-w-screen-lg w-full">
+      <div className="flex">
+        <div className="flex-1">Name</div>
+        <div className="flex-1">ID</div>
+        <div className="flex-1">Created at</div>
+        <div className="flex-1">Webhook URL</div>
+        <div className="flex-1">Go</div>
+      </div>
+      {zaps.map((z) => (
+        <div key={z.id} className="flex border-b border-t py-4">
+          <div className="flex-1 flex">
+            <Image
+              src={z.trigger.type.image}
+              alt={z.trigger.type.name}
+              className="w-[30px] h-[30px]"
+            />{" "}
+            {z.actions.map((x) => (
+              <Image
+                key={x.id}
+                src={x.type.image}
+                alt={x.type.name}
+                className="w-[30px] h-[30px]"
+              />
+            ))}
+          </div>
+          <div className="flex-1">{z.id}</div>
+          <div className="flex-1">Nov 13, 2023</div>
+          <div className="flex-1">{`${HOOKS_URL}/hooks/catch/1/${z.id}`}</div>
+          <div className="flex-1">
+            <LinkButton
+              onClick={() => {
+                router.push("/zap/" + z.id);
+              }}
+            >
+              Go
+            </LinkButton>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
