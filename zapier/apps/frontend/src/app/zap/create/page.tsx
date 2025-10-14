876e7cbb09523905a735bb821fad1e0035747659
KRRISH AGARWAL <krrishapj@gmail.com>
2025-10-15 01:50:20 +0530

    added zap/create FE bit

---
+++ zapier/apps/frontend/src/app/zap/create/page.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { ZapCell } from "@/components/ZapCell";
+import { PrimaryButton } from "@/components/buttons/PrimaryButton";
+import { useState } from "react";
+
+export default function Page() {
+  const [selectedTrigger, setSelectedTrigger] = useState<{
+    id: string;
+    name: string;
+  }>();
+
+  const [selectedActions, setSelectedActions] = useState<
+    {
+      index: number;
+      availableActionId: string;
+      availableActionName: string;
+      metadata: unknown;
+    }[]
+  >([]);
+
+  return (
+    <div>
+      {/* <Appbar /> */}
+      <div className="w-full min-h-screen bg-slate-200 flex flex-col justify-center">
+        <div className="flex justify-center w-full">
+          <ZapCell
+            onClick={() => {}}
+            name={selectedTrigger?.name ? selectedTrigger.name : "Trigger"}
+            index={1}
+          />
+        </div>
+        <div className="w-full pt-2 pb-2">
+          {selectedActions.map((action, index) => (
+            <div key={index} className="pt-2 flex justify-center">
+              <ZapCell
+                onClick={() => {}}
+                name={
+                  action.availableActionName
+                    ? action.availableActionName
+                    : "Action"
+                }
+                index={action.index}
+              />
+            </div>
+          ))}
+        </div>
+        <div className="flex justify-center">
+          <div>
+            <PrimaryButton
+              onClick={() => {
+                setSelectedActions((a) => [
+                  ...a,
+                  {
+                    index: a.length + 2,
+                    availableActionId: "",
+                    availableActionName: "",
+                    metadata: {},
+                  },
+                ]);
+              }}
+            >
+              <div className="text-2xl">+</div>
+            </PrimaryButton>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
